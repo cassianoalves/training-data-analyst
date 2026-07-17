595613eb6ef9e742a4ad32fdda0b50b7458b7af5
Cassiano Alves <cassianoa@juntoseguros.com>
2026-07-17 09:10:54 -0300

    Make IAM policy binding changes in task2a.sh for improved service account integration

--- quests/develop-apis-apigee/task2a.sh
+++ quests/develop-apis-apigee/task2a.sh
@@ -10,11 +10,13 @@ gcloud iam service-accounts create apigee-internal-access \
 gcloud run services add-iam-policy-binding simplebank-rest \
 --member="serviceAccount:apigee-internal-access@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com" \
 --role=roles/run.invoker --region=$CLOUDRUN_REGION \
+--platform=managed \
 --project=${GOOGLE_CLOUD_PROJECT}
 
 gcloud projects add-iam-policy-binding ${GOOGLE_CLOUD_PROJECT} \
 --member="serviceAccount:apigee-internal-access@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com" \
-  --role="roles/iam.serviceAccountTokenCreator"
+  --role="roles/iam.serviceAccountTokenCreator" \
+  --condition=none
 
 
 gcloud run services describe simplebank-rest --platform managed \
